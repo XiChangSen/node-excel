14e8450387ecef1f9a6bd455a3efea059e4c38af
XiChangSen <51038015+XiChangSen@users.noreply.github.com>
2020-09-17 21:36:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    inputDir: './assert/input', // 目标文件夹
+    mainFile: './assert/main.xlsx', // 主文件
+    outputFile: './assert/output/target.xlsx', // 输出文件路径
+    outputFile2: './assert/output/complete.xlsx', // 合并完成文件
+    tagStart: 5, // 标签从第几列开始
+}
